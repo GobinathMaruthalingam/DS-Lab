106dc9a204407910a2960883b82671f0186fca2f
GobinathMaruthalingam <m.gobinath2001@gmail.com>
2025-01-30 02:56:50 +0530

    Updated

--- Lab 2/Count_Freq.c
+++ Lab 2/Count_Freq.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int n;
+
+    // Taking input for the length of the array
+    printf("Enter length of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Generating & Printing random generated array
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 10 + 1;
+    }
+
+    printf("Generated array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Counting  & Printing the frequency of each number
+    int freq[10] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        freq[arr[i] - 1]++;
+    }
+
+    printf("Number frequencies:\n");
+    for (int i = 0; i < 10; i++)
+    {
+        if (freq[i] > 0)
+        {
+            printf("%d: %d times\n", i + 1, freq[i]);
+        }
+    }
+
+    return 0;
+}
--- Lab 2/Moving_Average.c
+++ Lab 2/Moving_Average.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int n, k;
+
+    // Taking input for length of the array and the frequency
+    printf("Enter length of array: ");
+    scanf("%d", &n);
+    
+    printf("Enter the frequency): ");
+    scanf("%d", &k);
+
+    if (k <= 0 || k > n) {
+        printf("Invalid window size!\n");
+        return 1;
+    }
+
+    int arr[n];
+
+    // Generating and printing random array
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 10 + 1;
+    }
+
+    printf("Generated array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Calculating and printing the moving average
+    printf("Moving averages: \n");
+
+    // For first window (smaller than 'k')
+    for (int j = 1; j < k; j++) {
+        float sum = 0;
+        for (int i = 0; i < j; i++) {
+            sum += arr[i];
+        }
+        printf("%.2f ", sum / j);
+    }
+
+    // For the rest of the windows
+    for (int j = 0; j < n - k + 1; j++) {
+        float sum = 0;
+        for (int i = j; i < k + j; i++) {
+            sum += arr[i];
+        }
+        printf("%.2f ", sum / k);
+    }
+    printf("\n");
+
+    return 0;
+}

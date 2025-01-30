8e56021f10a3b24a3f87566779406ad23959f665
GobinathMaruthalingam <m.gobinath2001@gmail.com>
2025-01-30 08:38:13 +0530

    Updated

--- Lab 2/Finding_Kth_Largest.c
+++ Lab 2/Finding_Kth_Largest.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int n, k;
+
+    printf("Enter length of array: ");
+    scanf("%d", &n);
+
+    printf("Enter K: ");
+    scanf("%d", &k);
+
+    int arr[n];
+
+    // Generating a random array
+    printf("Array is: ");
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 10 + 1; // Numbers between 1 and 10
+        printf("%d ", arr[i]);
+    }
+
+    int min = arr[0];
+    int max = arr[0];
+
+    // Finding the minimum value in the array
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    int temp;
+    int track = min - 1;
+    int count = 0;
+
+    if (k <= n) {
+        while (1) {
+            max = arr[0];
+            int maxIndex = 0;
+
+            // Finding the maximum value
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                    maxIndex = j;
+                }
+            }
+
+            // Track and update max element
+            if (track == max) {
+                count--;
+            }
+
+            track = max;
+            arr[maxIndex] = min - 1;
+            count++;
+
+            if (count >= k) {
+                break;
+            }
+        }
+
+        // Final Output
+        printf("\nKth largest value is: %d\n", max);
+    } else {
+        printf("\nInvalid Input for K\n");
+    }
+
+    return 0;
+}
--- Lab 2/Removing_Duplicates.c
+++ Lab 2/Removing_Duplicates.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int n, i, j, k;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Generating random array
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 10 + 1;
+    }
+
+    // Printing original array
+    printf("Original array: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    // Removing duplicates
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n;)
+        {
+            if (arr[i] == arr[j])
+            {
+                // Shifting elements to left to remove duplicate
+                for (k = j; k < n - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+                n--; // Reducing array size
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+
+    // Printing array after removing duplicates
+    printf("\nArray after removing duplicates: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}

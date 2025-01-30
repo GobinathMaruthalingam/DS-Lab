c663e9a40fe4afb60ccca16f09ec0433847abfc9
GobinathMaruthalingam <m.gobinath2001@gmail.com>
2025-01-30 08:16:49 +0530

    Updated

--- Lab 2/Insertion_Sort.c
+++ Lab 2/Insertion_Sort.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int n, i, j, temp, count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Generating random array
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 10 + 1;
+    }
+
+    // Printing original array
+    printf("Array: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    // Insertion Sort
+    for (i = 1; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j]; // Shift element right
+            j--;
+            count++;
+        }
+        arr[j + 1] = temp;
+    }
+
+    // Printing sorted array
+    printf("\n\nSorted array:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\nNumber of swaps: %d", count);
+
+    return 0;
+}
--- Lab 2/Sort_by_Freq.c
+++ Lab 2/Sort_by_Freq.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define MAX_VALUE 10
+
+int compare(const void *a, const void *b)
+{
+    return *(int *)b - *(int *)a; // Sort in descending order
+}
+
+int main()
+{
+    int n, arr[100], freq[MAX_VALUE + 1] = {0};
+
+    printf("Enter array size: ");
+    scanf("%d", &n);
+
+    printf("Generated array: ");
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % MAX_VALUE + 1;
+        printf("%d ", arr[i]);
+        freq[arr[i]]++;
+    }
+    printf("\n");
+
+    int sortedArr[100], index = 0;
+    for (int i = 1; i <= MAX_VALUE; i++)
+        for (int j = 0; j < freq[i]; j++)
+            sortedArr[index++] = i;
+
+    qsort(sortedArr, index, sizeof(int), compare);
+
+    printf("Sorted by frequency: ");
+    for (int i = 0; i < index; i++)
+        printf("%d ", sortedArr[i]);
+
+    return 0;
+}

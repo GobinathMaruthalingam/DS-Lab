a581a5d16baed5aaf07677010779aa5f1300b3eb
GobinathMaruthalingam <m.gobinath2001@gmail.com>
2025-04-25 00:21:50 +0530

    Files Updated

--- Lab 9/3.c
+++ Lab 9/3.c
@@ -3,3 +3,117 @@
 Implement a BST with following functionalities
 a) Insertion b) Deletion c) Search
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* nNode(int value) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = value;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+Node* insert(Node* root, int value) {
+    if (root == NULL)
+        return nNode(value);
+    
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+    
+    return root;
+}
+
+Node* search(Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+    
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+// Finding minimum value node
+Node* findMin(Node* root) {
+    while (root->left != NULL)
+        root = root->left;
+    return root;
+}
+
+Node* delete(Node* root, int key) {
+    if (root == NULL) return NULL;
+
+    if (key < root->data)
+        root->left = delete(root->left, key);
+    else if (key > root->data)
+        root->right = delete(root->right, key);
+    else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = delete(root->right, temp->data);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 90);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("Inorder: ");
+    inorder(root);
+    printf("\n");
+
+    root = delete(root, 70);
+    printf("Inorder after deleting 70: ");
+    inorder(root);
+    printf("\n");
+
+    int key = 30;
+    if (search(root, key))
+        printf("Found: %d\n", key);
+    else
+        printf("%d Not found\n", key);
+
+    return 0;
+}
+
+/*
+Output:
+Inorder: 30 40 50 60 70 80 90 
+Inorder after deleting 70: 30 40 50 60 80 90 
+Found: 30
+*/
--- Lab 9/4.c
+++ Lab 9/4.c
@@ -1,3 +1,111 @@
 /*
 4) Reconstruct a Binary tree for the given Inorder, Preorder (or Postorder).
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    char data;
+    struct Node *left, *right;
+} Node;
+
+Node *nNode(char data) {
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+int findIndex(char arr[], int start, int end, char value) {
+    for (int i = start; i <= end; i++) {
+        if (arr[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+Node *usingPre(char in[], char pre[], int start, int end, int *index) {
+    if (start > end)
+        return NULL;
+
+    char curr = pre[(*index)++];
+    Node *root = nNode(curr);
+
+    if (start == end)
+        return root;
+
+    int mid = findIndex(in, start, end, curr);
+    root->left = usingPre(in, pre, start, mid - 1, index);
+    root->right = usingPre(in, pre, mid + 1, end, index);
+
+    return root;
+}
+
+Node *usingPost(char in[], char post[], int start, int end, int *index) {
+    if (start > end)
+        return NULL;
+
+    char curr = post[(*index)--];
+    Node *root = nNode(curr);
+
+    if (start == end)
+        return root;
+
+    int mid = findIndex(in, start, end, curr);
+    root->right = usingPost(in, post, mid + 1, end, index);
+    root->left = usingPost(in, post, start, mid - 1, index);
+
+    return root;
+}
+
+void inorder(Node *root) {
+    if (root) {
+        inorder(root->left);
+        printf("%c ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    char inord[] = {'D', 'B', 'E', 'A', 'F', 'C'};
+    char preord[] = {'A', 'B', 'D', 'E', 'C', 'F'};
+    char postord[] = {'D', 'E', 'B', 'F', 'C', 'A'};
+    int len = 6;
+    int choice;
+
+    while (1) {
+        printf("\n--- Tree Reconstruction ---\n");
+        printf("1. Build from Inorder + Preorder\n");
+        printf("2. Build from Inorder + Postorder\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        Node *root = NULL;
+
+        if (choice == 1) {
+            int preIndex = 0;
+            root = usingPre(inord, preord, 0, len - 1, &preIndex);
+            printf("Inorder traversal: ");
+            inorder(root);
+            printf("\n");
+        }
+        else if (choice == 2) {
+            int postIndex = len - 1;
+            root = usingPost(inord, postord, 0, len - 1, &postIndex);
+            printf("Inorder traversal: ");
+            inorder(root);
+            printf("\n");
+        }
+        else if (choice == 3) {
+            printf("Exiting...\n");
+            break;
+        }
+        else {
+            printf("Invalid choice. Please try again.\n");
+        }
+    }
+
+    return 0;
+}
